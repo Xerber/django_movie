74fbdf5ab9a53d3b92024032d4b51d0586506a30
Igor Dziuba <cherobuk.91@gmail.com>
2022-05-17 01:38:35 +0300

    Created output of screenshots from the movie. Included the form of comments to the film and their output

--- django_movie/movies/views.py
+++ django_movie/movies/views.py
@@ -1,6 +1,7 @@
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, View
 from .models import Movie
-
+from django.shortcuts import redirect
+from .forms import ReviewForm
 
 # Create your views here.
 class MovieView(ListView):
@@ -13,3 +14,14 @@ class MovieDetailView(DetailView):
   '''Полное описание фильма'''
   model = Movie
   slug_field = "url"
+
+class AddReview(View):
+  '''Отзывы'''
+  def post(self,request,pk):
+    form = ReviewForm(request.POST)
+    movie = Movie.objects.get(id=pk)
+    if form.is_valid:
+      form = form.save(commit=False)
+      form.movie = movie
+      form.save()
+    return redirect(movie.get_absolute_url())
