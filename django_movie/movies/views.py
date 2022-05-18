301cfe9b6aa612f8410fc70009f2b1343491daaf
Igor Dziuba <cherobuk.91@gmail.com>
2022-05-19 00:40:43 +0300

    creating a comment on a comment and inferring the nested type

--- django_movie/movies/views.py
+++ django_movie/movies/views.py
@@ -20,8 +20,10 @@ class AddReview(View):
   def post(self,request,pk):
     form = ReviewForm(request.POST)
     movie = Movie.objects.get(id=pk)
-    if form.is_valid:
+    if form.is_valid():
       form = form.save(commit=False)
+      if request.POST.get("parent", None):
+        form.parent_id = int(request.POST.get("parent"))
       form.movie = movie
       form.save()
     return redirect(movie.get_absolute_url())
